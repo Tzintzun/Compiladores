02affd2ffecce43207d935cc216d3e74a419ce03
Diana <dianapaodcs@gmail.com>
2021-09-16 15:09:31 -0500

    class AnalizLexico y final de AFD

--- AnalizadorLexico/AnalizadorLexico/ClaseAFD.cs
+++ AnalizadorLexico/AnalizadorLexico/ClaseAFD.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AnalizadorLexico
 {
@@ -37,6 +38,57 @@ namespace AnalizadorLexico
             NumEstados = NumeroDeEstados;
             IdAFD = IdAutFD;
             AFD.ConjAFDs.Add(this);
+        }
+
+        public void GuardarADFenArchivo(string NombreArchivo)
+        {
+            using (StreamWriter writer = new StreamWriter(NombreArchivo, false))
+            {
+                writer.WriteLine(this.NumEstados.ToString());
+
+                for(int i = 0; i < NumEstados; i++)
+                {
+                    for(int j = 0; j < 257; j++)
+                    {
+                        writer.Write(TablaAFD[i, j]);
+                        if (j != 256)
+                            writer.WriteLine(";");
+                    }
+                    writer.WriteLine();
+                }
+                writer.Close();
+            }
+        }
+
+        public void LeerAFDdeArchivo(string NombArchivo, int IdentifAFD)
+        {
+            int IdEdo;
+            int k;
+            string Renglon;
+            string[] ValoresRenglon = new string[257];
+
+            using (StreamReader reader = new StreamReader(NombArchivo))
+            {
+                IdEdo = 0;
+                Renglon = reader.ReadLine();
+                this.NumEstados = int.Parse(Renglon);
+                this.TablaAFD = new int[this.NumEstados, 257];
+
+                while (!reader.EndOfStream)
+                {
+                    Renglon = reader.ReadLine();
+                    ValoresRenglon = Renglon.Split(';');
+                    for (k = 0; k < 257; k++)
+                        this.TablaAFD[IdEdo, k] = int.Parse(ValoresRenglon[k]);
+
+                    IdEdo++;
+                }
+            }
+
+            this.NumEstados = IdEdo;
+            this.IdAFD = IdentifAFD;
+            AFD.ConjAFDs.Add(this);
+            return;
         }
     }
 }
