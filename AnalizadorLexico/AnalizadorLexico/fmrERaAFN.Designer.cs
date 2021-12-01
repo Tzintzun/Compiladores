7f622dc95d93fc10ecf0829c7353da636e851331
Tzintzun <davidtztz@hotmail.com>
2021-11-30 23:09:04 -0600

    arreglado id er a afn

--- AnalizadorLexico/AnalizadorLexico/fmrERaAFN.Designer.cs
+++ AnalizadorLexico/AnalizadorLexico/fmrERaAFN.Designer.cs
@@ -40,20 +40,24 @@ namespace AnalizadorLexico
             this.label2 = new System.Windows.Forms.Label();
             this.txt_afn_id = new System.Windows.Forms.TextBox();
             this.btn_crear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.afds = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.lbl_file_name);
             this.groupBox1.Controls.Add(this.afd_id);
             this.groupBox1.Controls.Add(this.txt_afd_id);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(25, 22);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(230, 122);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(230, 145);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Analizador Léxico";
@@ -61,7 +65,7 @@ namespace AnalizadorLexico
             // lbl_file_name
             // 
             this.lbl_file_name.AutoSize = true;
-            this.lbl_file_name.Location = new System.Drawing.Point(88, 97);
+            this.lbl_file_name.Location = new System.Drawing.Point(93, 121);
             this.lbl_file_name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_file_name.Name = "lbl_file_name";
             this.lbl_file_name.Size = new System.Drawing.Size(23, 13);
@@ -72,7 +76,7 @@ namespace AnalizadorLexico
             // afd_id
             // 
             this.afd_id.AutoSize = true;
-            this.afd_id.Location = new System.Drawing.Point(20, 32);
+            this.afd_id.Location = new System.Drawing.Point(25, 56);
             this.afd_id.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.afd_id.Name = "afd_id";
             this.afd_id.Size = new System.Drawing.Size(66, 13);
@@ -81,8 +85,9 @@ namespace AnalizadorLexico
             // 
             // txt_afd_id
             // 
-            this.txt_afd_id.Location = new System.Drawing.Point(89, 29);
-            this.txt_afd_id.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_afd_id.Enabled = false;
+            this.txt_afd_id.Location = new System.Drawing.Point(94, 53);
+            this.txt_afd_id.Margin = new System.Windows.Forms.Padding(2);
             this.txt_afd_id.Name = "txt_afd_id";
             this.txt_afd_id.Size = new System.Drawing.Size(76, 20);
             this.txt_afd_id.TabIndex = 7;
@@ -90,8 +95,9 @@ namespace AnalizadorLexico
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 59);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(96, 83);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 35);
             this.button1.TabIndex = 5;
@@ -106,8 +112,8 @@ namespace AnalizadorLexico
             // txt_exp
             // 
             this.txt_exp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_exp.Location = new System.Drawing.Point(170, 179);
-            this.txt_exp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_exp.Location = new System.Drawing.Point(170, 218);
+            this.txt_exp.Margin = new System.Windows.Forms.Padding(2);
             this.txt_exp.Name = "txt_exp";
             this.txt_exp.Size = new System.Drawing.Size(354, 23);
             this.txt_exp.TabIndex = 10;
@@ -116,7 +122,7 @@ namespace AnalizadorLexico
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 179);
+            this.label1.Location = new System.Drawing.Point(68, 218);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 13);
@@ -127,7 +133,7 @@ namespace AnalizadorLexico
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(106, 214);
+            this.label2.Location = new System.Drawing.Point(106, 253);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 13);
@@ -137,8 +143,8 @@ namespace AnalizadorLexico
             // 
             // txt_afn_id
             // 
-            this.txt_afn_id.Location = new System.Drawing.Point(169, 214);
-            this.txt_afn_id.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_afn_id.Location = new System.Drawing.Point(169, 253);
+            this.txt_afn_id.Margin = new System.Windows.Forms.Padding(2);
             this.txt_afn_id.Name = "txt_afn_id";
             this.txt_afn_id.Size = new System.Drawing.Size(54, 20);
             this.txt_afn_id.TabIndex = 13;
@@ -146,8 +152,8 @@ namespace AnalizadorLexico
             // 
             // btn_crear
             // 
-            this.btn_crear.Location = new System.Drawing.Point(48, 259);
-            this.btn_crear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_crear.Location = new System.Drawing.Point(48, 298);
+            this.btn_crear.Margin = new System.Windows.Forms.Padding(2);
             this.btn_crear.Name = "btn_crear";
             this.btn_crear.Size = new System.Drawing.Size(72, 35);
             this.btn_crear.TabIndex = 14;
@@ -155,19 +161,49 @@ namespace AnalizadorLexico
             this.btn_crear.UseVisualStyleBackColor = true;
             this.btn_crear.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "AFD\'s cargados:";
+            // 
+            // afds
+            // 
+            this.afds.FormattingEnabled = true;
+            this.afds.Location = new System.Drawing.Point(170, 184);
+            this.afds.Name = "afds";
+            this.afds.Size = new System.Drawing.Size(121, 21);
+            this.afds.TabIndex = 16;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(28, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Cargar Desde Archivo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.ambioOrigenAFD);
+            // 
             // fmrERaAFN
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
             this.ControlBox = false;
+            this.Controls.Add(this.afds);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btn_crear);
             this.Controls.Add(this.txt_afn_id);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txt_exp);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "fmrERaAFN";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.groupBox1.ResumeLayout(false);
@@ -190,5 +226,8 @@ namespace AnalizadorLexico
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txt_afn_id;
         private System.Windows.Forms.Button btn_crear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox afds;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- AnalizadorLexico/AnalizadorLexico/fmrERaAFN.cs
+++ AnalizadorLexico/AnalizadorLexico/fmrERaAFN.cs
@@ -35,17 +35,64 @@ namespace AnalizadorLexico
             string sigma = txt_exp.Text;
             int id_afd = Int32.Parse(txt_afd_id.Text);
             int id_afn = Int32.Parse(txt_afn_id.Text);
-            er_afd = new ER_AFN(sigma, FileAFD, id_afd);
+
+            foreach (AFN n in AFN.ConjuntoAFNs)
+            {
+                if (n.idAFN == id_afn)
+                {
+                    MessageBox.Show("ID del AFN ya seleccionado, seleccione otro", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+            if (checkBox1.Checked)
+            {
+                foreach (AFD n in AFD.conjAFDs)
+                {
+                    if (n.IdAFD == id_afd)
+                    {
+                        MessageBox.Show("ID del AFD ya seleccionado, seleccione otro", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                }
+
+
+                er_afd = new ER_AFN(sigma, FileAFD, id_afd);
+            }
+            else
+            {
+                AFD aux = null;
+                if (afds.SelectedIndex < 0)
+                {
+                    MessageBox.Show("Selecciona un AFD", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                foreach (AFD f in AFD.conjAFDs)
+                {
+                    if(f.IdAFD == (int)afds.SelectedItem)
+                    {
+                        aux = f;
+                        break;
+                    }
+                }
+                if(aux == null)
+                {
+                    MessageBox.Show("AFD no encontrado", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                er_afd = new ER_AFN(sigma, aux) ;
+            }
             bool result = er_afd.IniConversion();
 
-            if(result)
+            if (result)
             {
-                er_afd.AgregarAFN(id_afn);
                 MessageBox.Show("El AFN fue creado exitosamente", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }else
+                er_afd.AgregarAFN(id_afn);
+            }
+            else
             {
                 MessageBox.Show("AFN no se pudo crear", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -85,6 +132,28 @@ namespace AnalizadorLexico
         private void txt_afn_id_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void ambioOrigenAFD(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                afds.Enabled = false;
+                txt_afd_id.Enabled = true;
+                button1.Enabled = true;
+            }
+            else
+            {
+                afds.Enabled = true;
+                txt_afd_id.Enabled = false;
+                button1.Enabled = false;
+
+                afds.Items.Clear();
+                foreach(AFD f in AFD.conjAFDs)
+                {
+                    afds.Items.Add(f.IdAFD);
+                }
+            }
         }
     }
 }
